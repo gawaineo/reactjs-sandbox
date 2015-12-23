225455bf54ccb7d615f5619e7155882302156873
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2015-12-23 14:08:20 -0500

    add reactjs work completed

---
+++ app/static/HelloUser.js
@@ -0,0 +1,99 @@
+
+var HelloUser = React.createClass({
+  getInitialState: function() {
+      return {
+        username: '@gawaineo'
+      }
+  },
+
+  handleChange: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        Hello {this.props.name} <br />
+        // Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+      </div>
+    )
+  }
+});
+
+
+var FriendsContainer =  React.createClass({
+  getInitialState: function (){
+    return {
+      name: 'Bruce Wayne',
+      friends: ['Clark Kent', 'Oliver Queen', 'Barry Allen']
+    }
+  },
+
+  addFriend: function(friend) {
+    this.setState({
+      friends: this.state.friends.concat([friend])
+    });
+  },
+
+  render: function (){
+    return (
+      <div>
+        <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+});
+
+var AddFriend = React.createClass({
+  getInitialState: function() {
+    return {
+      newFriend: ''
+    }
+  },
+  handleAddNew: function() {
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ''
+    })
+  },
+  updateNewFriend: function(e) {
+    this.setState({
+      newFriend: e.target.value
+    });
+  },
+
+  render: function(){
+    return (
+      <div>
+        <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend} />
+        <button type="submit" onClick={this.handleAddNew}> Add Friend</button>
+      </div>
+    )
+  }
+});
+
+
+var ShowList = React.createClass({
+  render: function () {
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul> {listItems} </ul>
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render(
+  <FriendsContainer />,
+  document.getElementById('content')
+);
